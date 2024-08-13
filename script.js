b8a90b55841ff84b6de50550492b7d62f112beb1
Dawiddek <150728883+Dawiddek@users.noreply.github.com>
2024-08-13 17:25:29 +0200

    Poprawienie działaności navbaru

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+const menus = document.querySelector("nav ul");
+const menuBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav ul li a');
+menuBtn.addEventListener("click", () => {
+    menus.classList.toggle("display");
+});
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active')
-            })
-        }
-    })
-}
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+closeBtn.addEventListener("click", () => {
+    menus.classList.remove("display");
+});
