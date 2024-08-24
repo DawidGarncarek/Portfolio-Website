70656e8f6b2f16357dcc10c20d56867600c90280
Dawiddek <150728883+Dawiddek@users.noreply.github.com>
2024-08-25 00:04:54 +0200

    Usprawnienia i poprawki

--- main.ts
+++ main.ts
@@ -8,18 +8,28 @@ import './style.css'
     document.addEventListener('DOMContentLoaded', () => {
         const interBubble = document.querySelector<HTMLDivElement>('.interactive')!;
 
+        if (!interBubble) {
+            console.log('Element .interactive nie został znaleziony!');
+            return;
+        } else {
+            console.log('Element .interactive został znaleziony.');
+        }
+
     function move() {
         curX += (tgX - curX) / 20;
         curY += (tgY - curY) / 20;
+        
+        // Przeniesienie elementu względem jego rodzica
         interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-        requestAnimationFrame(() => {
-            move();
-        });
+
+        requestAnimationFrame(move);
     }
 
     window.addEventListener('mousemove', (event) => {
-        tgX = event.clientX;
-        tgY = event.clientY;
+        tgX = event.clientX - (interBubble.clientWidth / 2); 
+        tgY = event.clientY - (interBubble.clientHeight / 2); 
+
+        console.log(`tgX: ${tgX}, tgY: ${tgY}`);
     });
 
     move();
